4218556672ecb550d9e403ceaec129c10be4622e
Sam W <sam@sammymayhem.com>
2023-01-16 11:49:03 -0800

    Added logo in navbar, positioned links, added collapsable nav for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,23 @@
 import './Header.css';
+import { Navbar, Nav, Container, Image } from 'react-bootstrap';
+import logo from '../../assets/images/poke-logo.png';
+
 
 function Header() {
     return (
         <div className='header'>
-
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home"><Image src={logo} className='logo'></Image></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto nav-links">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
