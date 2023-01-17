339b30398cbc3f6152d177dabed149cac7a89fb9
Sam W <sam@sammymayhem.com>
2023-01-16 16:29:20 -0800

    Added API and connected to input box. Still need to organize information

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import { InputGroup, Form, Button, Card, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
 import './Home.css';
 
 function Home() {
+
+    const [name, setname] = useState("");
+    const [Find, setFind] = useState("");
+    const [Img, setImg] = useState("");
+    const [Type, setType] = useState("");
+
+    useEffect(() => {
+        async function getData() {
+            let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${Find}`);
+            console.log(res);
+            setImg(res.data.sprites.front_default);
+            setType(res.data.types[0].type.name);
+        }
+
+        getData();
+    }, [Find]);
+
+    const Typename = (event) => {
+        setname(event.target.value);
+    };
+
+    const Search = () => {
+        if (name !== "") setFind(name);
+        setname("");
+    };
+
     return (
         <div className='home'>
             <div className='poke-input'>
@@ -10,22 +38,24 @@ function Home() {
                         placeholder="Enter pokemon"
                         aria-label="Enter pokemon"
                         aria-describedby="basic-addon2"
+                        type='text'
+                        onChange={Typename}
+                        value={name}
                     />
-                    <Button variant="outline-secondary" id="button-addon2" className='search-btn'>
+                    <Button variant="outline-secondary" id="button-addon2" className='search-btn' onClick={Search}>
                         Go!
                     </Button>
                 </InputGroup>
             </div>
 
-        {/* This will need to be mapped once the api is added */}
+            {/* This will need to be mapped once the api is added */}
             <div className='poke-card'>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                    <Card.Img variant="top" src={`${Img}`} />
                     <Card.Body>
-                        <Card.Title>Pokemon Name</Card.Title>
-                        <Card.Text>
-                            Poke Info: Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
+                        <Card.Title className='name'>{Find.toUpperCase()}</Card.Title>
+                        <Card.Text className='type'>
+                            {Type}
                         </Card.Text>
                     </Card.Body>
                     <ListGroup className="list-group-flush">
